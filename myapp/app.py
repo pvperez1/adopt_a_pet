d91e2369f43663f87a1b382ead952d963abd6485
PJ Perez <pvperez1@up.edu.ph>
2021-12-09 21:59:17 +0800

    Step 17 complete

--- myapp/app.py
+++ myapp/app.py
@@ -19,15 +19,22 @@ def animals(pet_type):
     html = "<h1>List of {}</h1>".format(pet_type)
     html += "<ul>"
 
-    for pet in pets[pet_type]:
-        html += "<li>{n}</li>".format(n = pet["name"])
+    for i,pet in enumerate(pets[pet_type]):
+        html += "<li><a href='/animals/{}/{}'>{n}</a></li>".format(pet_type,i,n = pet["name"])
 
     html += "</ul>"
 
 
     return html
 
-
+@app.route('/animals/<pet_type>/<int:pet_id>')
+def pet(pet_type, pet_id):
+    pet = pets[pet_type][pet_id]
+    html = "<h1>{}</h1>".format(pet['name'])
+    html += "<img src={}>".format(pet['url'])
+    html += "<p>{}</p>".format(pet['description'])
+    html += "<ul><li>Breed: {}</li><li>Age: {}</li></ul>".format(pet['breed'], pet['age'])
+    return html
 
 if __name__ == "__main__":
     app.run(debug=True)
